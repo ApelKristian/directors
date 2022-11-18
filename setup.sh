21e7cce43c15578ed745634d49ecc79bae2674bf
Kristian Apel <73107850+ApelKristian@users.noreply.github.com>
2022-11-18 13:01:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,5 +37,27 @@ sudo gdebi rstudio-server-2022.07.2-576-amd64.deb
 adduser defuser
 usermod -aG sudo defuser
 
+#install docker
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker defuser
+
+
 
 #go manual from here
